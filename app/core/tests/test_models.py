85114fc895c7b570f19ee821089685157d1d8c6e
moviefan322 <philman202@yahoo.com>
2024-07-07 10:49:15 -0400

    adds superuser support

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,13 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_superuser)
