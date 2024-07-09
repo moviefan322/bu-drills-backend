8ad543903c5195c8ed4f006a0a10c1c4a0fbfa29
moviefan322 <philman202@yahoo.com>
2024-07-09 14:47:32 -0400

    fixes flake8 errors

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
 
-)
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path
 
@@ -26,7 +23,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
-         SpectacularSwaggerView.as_view(url_name='api-schema'),
-         name='api-docs'
-    )
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs'
+    ),
 ]
