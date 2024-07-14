e1f439820a6df81bddb6acd0b07443cfc33f0ead
moviefan322 <philman202@yahoo.com>
2024-07-14 17:26:03 -0400

    adds test for create_drillScore

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,9 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+import datetime
+
 
 class ModelTests(TestCase):
     """Test models."""
@@ -41,3 +44,23 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_superuser)
+
+    def test_create_drillScore(self):
+        """Test creating a drillScore is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        drillScore = models.DrillScore.objects.create(
+            user=user,
+            drillId=123,
+            score=5,
+            maxScore=10,
+            date=datetime(2021, 1, 1),
+        )
+
+        self.assertEqual(drillScore.user, user, "The user should match the one created")
+        self.assertEqual(drillScore.drillId, 123, "The drillId should be 123")
+        self.assertEqual(drillScore.score, 5, "The score should be 5")
+        self.assertEqual(drillScore.maxScore, 10, "The maxScore should be 10")
+        self.assertEqual(drillScore.date, datetime(2021, 1, 1), "The date should be January 1, 2021")
