37a1db56fcfa9cf234bb0d8443baa20e60539e97
moviefan322 <philman202@yahoo.com>
2024-08-07 23:51:40 -0400

    writes serializers

---
+++ app/drill/serializers.py
@@ -0,0 +1,26 @@
+"""
+Serializers for drill API.
+"""
+from rest_framework import serializers
+
+from core.models import Drill
+
+
+class DrillSerializer(serializers.ModelSerializer):
+    """Serializer for drill objects"""
+
+    class Meta:
+        model = Drill
+        fields = [
+            'id', 'name', 'maxScore', 'instructions', 'image',
+            'type', 'skills', 'attempts', 'layouts',
+            'layoutsMaxScore', 'createdAt']
+        read_only_fields = ['id', 'createdAt']
+
+
+class DrillDetailSerializer(DrillSerializer):
+    """Serialize a drill detail"""
+
+    class Meta(DrillSerializer.Meta):
+        fields = DrillSerializer.Meta.fields
+        read_only_fields = DrillSerializer.Meta.read_only_fields
