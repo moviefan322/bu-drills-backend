447abfdb0a6444c46d9c7ae21775408fc16e875c
moviefan322 <philman202@yahoo.com>
2024-08-16 02:24:28 -0400

    all tests pass

--- app/drillsetScore/tests/test_drillsetScore_api.py
+++ app/drillsetScore/tests/test_drillsetScore_api.py
@@ -1,219 +1,223 @@
-# """
-# Tests for drillsetScore APIs.
-# """
+"""
+Tests for drillsetScore APIs.
+"""
 
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# from rest_framework import status
-# from rest_framework.test import APIClient
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# from core.models import DrillSet, Drill, DrillSetScore, DrillScore
+from core.models import DrillSet, Drill, DrillSetScore, DrillScore
 
-# from drillsetScore.serializers import DrillSetScoreSerializer
+from drillsetScore.serializers import DrillSetScoreSerializer
 
-# DRILLSETSCORE_URL = reverse('drillsetScore:drillsetscore-list')
+import random
 
-# def detail_url(drillSetScoreId):
-#     """Return drillsetScore detail URL"""
-#     return reverse('drillsetScore:drillsetscore-detail', args=[drillSetScoreId])
 
-# def create_user(**params):
-#     """Create and return a sample user"""
-#     return get_user_model().objects.create_user(**params)
+DRILLSETSCORE_URL = reverse('drillsetScore:drillsetscore-list')
 
-# def create_drill(createdBy, **params):
-#     """Create and return a sample drill"""
-#     defaults = {
-#         'name': 'Drill 1',
-#         'maxScore': 10,
-#         'instructions': 'Test instructions',
-#         'type': 'standard',
-#         'skills': ['potting', 'position', 'aim'],
-#     }
-#     defaults.update(params)
-#     return Drill.objects.create(createdBy=createdBy, **defaults)
+def detail_url(drillSetScoreId):
+    """Return drillsetScore detail URL"""
+    return reverse('drillsetScore:drillsetscore-detail', args=[drillSetScoreId])
 
-# def create_drillset(createdBy, **params):
-#     """Create and return a sample drillset"""
-#     drill1 = create_drill(createdBy=createdBy)
-#     drill2 = create_drill(createdBy=createdBy)
-#     drill3 = create_drill(createdBy=createdBy)
+def create_user(**params):
+    """Create and return a sample user with a unique email"""
+    unique_email = f"user{random.randint(1, 10000)}@example.com"
+    return get_user_model().objects.create_user(email=unique_email, **params)
+
+def create_drill(createdBy, **params):
+    """Create and return a sample drill"""
+    defaults = {
+        'name': 'Drill 1',
+        'maxScore': 10,
+        'instructions': 'Test instructions',
+        'type': 'standard',
+        'skills': ['potting', 'position', 'aim'],
+    }
+    defaults.update(params)
+    return Drill.objects.create(createdBy=createdBy, **defaults)
+
+def create_drillset(createdBy, **params):
+    """Create and return a sample drillset"""
+    drill1 = create_drill(createdBy=createdBy)
+    drill2 = create_drill(createdBy=createdBy)
+    drill3 = create_drill(createdBy=createdBy)
     
-#     drill_set = DrillSet.objects.create(
-#         name="Example Drill Set",
-#         user=createdBy
-#     )
-#     drill_set.drills.add(drill1, drill2, drill3)
+    drill_set = DrillSet.objects.create(
+        name="Example Drill Set",
+        createdBy=createdBy
+    )
+    drill_set.drills.add(drill1, drill2, drill3)
     
-#     return drill_set  # Return the created drill set
+    return drill_set  # Return the created drill set
 
-# def create_drillscore(drill, score, user, **params):
-#     """Create and return a sample drill score"""
-#     defaults = {
-#         'score': score,
-#         'maxScore': drill.maxScore,
-#         'user': user,
-#     }
-#     defaults.update(params)
-#     return DrillScore.objects.create(drill=drill, **defaults)
+def create_drillscore(drill, score, user, **params):
+    """Create and return a sample drill score"""
+    defaults = {
+        'score': score,
+        'maxScore': drill.maxScore,
+        'user': user,
+    }
+    defaults.update(params)
+    return DrillScore.objects.create(drill=drill, **defaults)
 
-# def create_drillsetscore(**params):
-#     """Create and return a sample drill set score"""
-#     user = create_user(
-#         email='example9@example.com',
-#         password='password123',
-#     )
-#     drill_set = create_drillset(createdBy=user)
+def create_drillsetscore(user, **params):
+    """Create and return a sample drill set score"""
+    drill_set = create_drillset(createdBy=user)
 
-#     scores = []
-#     for drill in drill_set.drills.all():
-#         score = create_drillscore(drill=drill, score=5, user=user)
-#         scores.append(score)
+    scores = []
+    for drill in drill_set.drills.all():
+        score = create_drillscore(drill=drill, score=5, user=user)
+        scores.append(score)
 
-#     drillset_score = DrillSetScore.objects.create(
-#         drill_set=drill_set,
-#     )
-#     drillset_score.scores.set(scores)
+    drillset_score = DrillSetScore.objects.create(
+        drill_set=drill_set,
+        user=user,
+        **params 
+    )
+    drillset_score.scores.set(scores)
 
-#     return drillset_score
+    return drillset_score
 
 
 
-# class PublicDrillSetScoreApiTests(TestCase):
-#     """Test the publicly available drillsetScore API"""
 
-#     def setUp(self):
-#         self.client = APIClient()
+class PublicDrillSetScoreApiTests(TestCase):
+    """Test the publicly available drillsetScore API"""
 
-#     def test_login_required(self):
-#         """Test that login is required for retrieving drillsetScores"""
-#         res = self.client.get(DRILLSETSCORE_URL)
+    def setUp(self):
+        self.client = APIClient()
 
-#         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_login_required(self):
+        """Test that login is required for retrieving drillsetScores"""
+        res = self.client.get(DRILLSETSCORE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-# class PrivateDrillSetScoreApiTests(TestCase):
-#     """Test the authorized user drillsetScore API"""
+class PrivateDrillSetScoreApiTests(TestCase):
+    """Test the authorized user drillsetScore API"""
 
-#     def setUp(self):
-#         self.client = APIClient()
-#         self.user = create_user(
-#             email='example@example.com',
-#             password='password123',
-#         )
-#         self.client.force_authenticate(self.user)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(
+            password='password123',
+        )
+        self.client.force_authenticate(self.user)
 
-#     def test_retrieve_drillsetScores(self):
-#         """Test retrieving drillsetScores"""
-#         create_drillsetscore(drillSet=create_drillset(createdBy=self.user), user=self.user)
-#         create_drillsetscore(drillSet=create_drillset(createdBy=self.user), user=self.user)
+    def test_retrieve_drillsetScores(self):
+        """Test retrieving drillsetScores"""
+        create_drillsetscore(user=self.user)
+        create_drillsetscore(user=self.user)
 
-#         res = self.client.get(DRILLSETSCORE_URL)
+        res = self.client.get(DRILLSETSCORE_URL)
 
-#         drillsetScores = DrillSetScore.objects.all().order_by('id')
+        drillsetScores = DrillSetScore.objects.filter(user=self.user).order_by('-id')
+        serializer = DrillSetScoreSerializer(drillsetScores, many=True)
 
-#         serializer = DrillSetScoreSerializer(drillsetScores, many=True)
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertEqual(res.data, serializer.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-#     def test_retrieve_drillsetScore_detail(self):
-#         """Test retrieving a drillsetScore detail"""
-#         drillsetScore = create_drillsetscore(drillSet=create_drillset(createdBy=self.user), user=self.user)
 
-#         url = detail_url(drillsetScore.id)
-#         res = self.client.get(url)
+    def test_retrieve_drillsetScore_detail(self):
+        """Test retrieving a drillsetScore detail"""
+        drillsetScore = create_drillsetscore(user=self.user)
 
-#         serializer = DrillSetScoreSerializer(drillsetScore)
-#         self.assertEqual(res.data, serializer.data)
+        url = detail_url(drillsetScore.id)
+        res = self.client.get(url)
 
-#     def test_create_drillsetScore(self):
-#         """Test creating a drillsetScore"""
-#         drillset = create_drillset(createdBy=self.user)
-#         payload = {
-#             'drillSet': drillset.id,
-#             'user': self.user.id,
-#         }
-#         res = self.client.post(DRILLSETSCORE_URL, payload)
+        serializer = DrillSetScoreSerializer(drillsetScore)
+        self.assertEqual(res.data, serializer.data)
 
-#         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-#         drillsetScore = DrillSetScore.objects.get(id=res.data['id'])
-#         self.assertEqual(drillsetScore.user, self.user)
-#         self.assertEqual(drillsetScore.drillSet, drillset)
+    def test_create_drillsetScore(self):
+        """Test creating a drillsetScore"""
+        drillset = create_drillset(createdBy=self.user)
+        payload = {
+            'drill_set': drillset.id,
+            'user': self.user.id,
+        }
+        res = self.client.post(DRILLSETSCORE_URL, payload)
 
-#     def test_create_drillsetScore_with_scores(self):
-#         """Test creating a drillsetScore with scores"""
-#         drillset = create_drillset(createdBy=self.user)
-#         drill1 = create_drill(createdBy=self.user)
-#         drill2 = create_drill(createdBy=self.user)
-#         drill3 = create_drill(createdBy=self.user)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        drillsetScore = DrillSetScore.objects.get(id=res.data['id'])
+        self.assertEqual(drillsetScore.user, self.user)
+        self.assertEqual(drillsetScore.drill_set, drillset)
 
-#         score1 = create_drillscore(drill1, 5, user=self.user)
-#         score2 = create_drillscore(drill2, 7, user=self.user)
-#         score3 = create_drillscore(drill3, 9, user=self.user)
+    def test_create_drillsetScore_with_scores(self):
+        """Test creating a drillsetScore with scores"""
+        drillset = create_drillset(createdBy=self.user)
+        drill1 = create_drill(createdBy=self.user)
+        drill2 = create_drill(createdBy=self.user)
+        drill3 = create_drill(createdBy=self.user)
 
-#         payload = {
-#             'drillSet': drillset.id,
-#             'user': self.user.id,
-#             'scores': [score1.id, score2.id, score3.id],
-#         }
-#         res = self.client.post(DRILLSETSCORE_URL, payload)
+        score1 = create_drillscore(drill1, 5, user=self.user)
+        score2 = create_drillscore(drill2, 7, user=self.user)
+        score3 = create_drillscore(drill3, 9, user=self.user)
 
-#         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-#         drillsetScore = DrillSetScore.objects.get(id=res.data['id'])
-#         self.assertEqual(drillsetScore.user, self.user)
-#         self.assertEqual(drillsetScore.drillSet, drillset)
-#         self.assertEqual(drillsetScore.scores.count(), 3)
+        payload = {
+            'drill_set': drillset.id,
+            'user': self.user.id,
+            'scores': [score1.id, score2.id, score3.id],
+        }
+        res = self.client.post(DRILLSETSCORE_URL, payload)
 
-#     def test_partial_update_drillsetScore(self):
-#         """Test updating a drillsetScore with patch"""
-#         drillsetScore = create_drillsetscore(drillSet=create_drillset(createdBy=self.user), user=self.user)
-#         drill1 = create_drill(createdBy=self.user)
-#         drill2 = create_drill(createdBy=self.user)
-#         drill3 = create_drill(createdBy=self.user)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        drillsetScore = DrillSetScore.objects.get(id=res.data['id'])
+        self.assertEqual(drillsetScore.user, self.user)
+        self.assertEqual(drillsetScore.drill_set, drillset)
+        self.assertEqual(drillsetScore.scores.count(), 3)
 
-#         score1 = create_drillscore(drill1, 5)
-#         score2 = create_drillscore(drill2, 7)
-#         score3 = create_drillscore(drill3, 9)
+    def test_partial_update_drillsetScore(self):
+        """Test updating a drillsetScore with patch"""
+        drillsetScore = create_drillsetscore(user=self.user)
+        drill1 = create_drill(createdBy=self.user)
+        drill2 = create_drill(createdBy=self.user)
+        drill3 = create_drill(createdBy=self.user)
 
-#         payload = {
-#             'scores': [score1.id, score2.id, score3.id],
-#         }
-#         url = detail_url(drillsetScore.id)
-#         self.client.patch(url, payload)
+        # Ensure to pass the user when creating drill scores
+        score1 = create_drillscore(drill1, 5, user=self.user)
+        score2 = create_drillscore(drill2, 7, user=self.user)
+        score3 = create_drillscore(drill3, 9, user=self.user)
 
-#         drillsetScore.refresh_from_db()
-#         self.assertEqual(drillsetScore.scores.count(), 3)
+        payload = {
+            'scores': [score1.id, score2.id, score3.id],
+        }
+        url = detail_url(drillsetScore.id)
+        self.client.patch(url, payload)
 
-#     def test_full_update_drillsetScore(self):
-#         """Test updating a drillsetScore with put"""
-#         drillsetScore = create_drillsetscore(drillSet=create_drillset(createdBy=self.user), user=self.user)
-#         drill1 = create_drill(createdBy=self.user)
-#         drill2 = create_drill(createdBy=self.user)
-#         drill3 = create_drill(createdBy=self.user)
+        drillsetScore.refresh_from_db()
+        self.assertEqual(drillsetScore.scores.count(), 3)
 
-#         score1 = create_drillscore(drill1, 5)
-#         score2 = create_drillscore(drill2, 7)
-#         score3 = create_drillscore(drill3, 9)
+    def test_full_update_drillsetScore(self):
+        """Test updating a drillsetScore with put"""
+        drillsetScore = create_drillsetscore(user=self.user)
+        drill1 = create_drill(createdBy=self.user)
+        drill2 = create_drill(createdBy=self.user)
+        drill3 = create_drill(createdBy=self.user)
 
-#         payload = {
-#             'drillSet': drillsetScore.drillSet.id,
-#             'user': drillsetScore.user,
-#             'scores': [score1.id, score2.id, score3.id],
-#         }
-#         url = detail_url(drillsetScore.id)
-#         self.client.put(url, payload)
+        score1 = create_drillscore(drill1, 5, user=self.user)
+        score2 = create_drillscore(drill2, 7, user=self.user)
+        score3 = create_drillscore(drill3, 9, user=self.user)
 
-#         drillsetScore.refresh_from_db()
-#         self.assertEqual(drillsetScore.scores.count(), 3)
+        payload = {
+            'drill_set': drillsetScore.drill_set.id,
+            'user': drillsetScore.user,
+            'scores': [score1.id, score2.id, score3.id],
+        }
+        url = detail_url(drillsetScore.id)
+        self.client.put(url, payload)
 
-#     def test_delete_drillsetScore(self):
-#         """Test deleting a drillsetScore"""
-#         drillsetScore = create_drillsetscore(drillSet=create_drillset(createdBy=self.user))
-#         url = detail_url(drillsetScore.id)
-#         res = self.client.delete(url)
+        drillsetScore.refresh_from_db()
+        self.assertEqual(drillsetScore.scores.count(), 3)
 
-#         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertEqual(DrillSetScore.objects.filter(id=drillsetScore.id).count(), 0)
+    def test_delete_drillsetScore(self):
+        """Test deleting a drillsetScore"""
+        drillsetScore = create_drillsetscore(user=self.user)
+        url = detail_url(drillsetScore.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(DrillSetScore.objects.filter(id=drillsetScore.id).count(), 0)
 
--- app/drillsetScore/views.py
+++ app/drillsetScore/views.py
@@ -4,18 +4,18 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import DrillSetScore
 from drillsetScore import serializers
-
+from .permissions import IsOwner 
 
 class DrillSetScoreViewSet(viewsets.ModelViewSet):
     """View for managing drillSetScore APIs"""
     serializer_class = serializers.DrillSetScoreSerializer
     queryset = DrillSetScore.objects.all()
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner] 
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(createdBy=self.request.user).order_by('-id')
+        return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def perform_create(self, serializer):
         """Create a new drillSetScore"""
